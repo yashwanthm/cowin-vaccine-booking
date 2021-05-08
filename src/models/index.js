fd1969505a40ebf5d0ba2185f8e534f1cc87c38a
cowin-check <83546235+cowin-check@users.noreply.github.com>
2021-05-08 10:17:49 +0530

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { Observable } from 'rxjs';
 import CryptoJS from 'crypto-js';
 const apipath = `https://cdn-api.co-vin.in/api`;
-const url = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin`
-const zurl = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict`
+const url = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByPin`
+const zurl = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict`
 const burl = `https://cdn-api.co-vin.in/api//v2/appointment/schedule`
 const secret = "U2FsdGVkX19mD56KTNfQsZgXJMwOG7u/6tuj0Qvil1LEjx783oxHXGUTDWYm+XMYVGXPeu+a24sl5ndEKcLTUQ==";
 const pollFreq = 5* 1000;
