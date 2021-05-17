54e32a5172f5621276dec369a2f90567ff7e5be3
Yashwanth M <yashwanth.m@eagleview.com>
2021-05-17 08:51:07 +0530

    unreg and reg

--- src/index.js
+++ src/index.js
@@ -19,6 +19,6 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.unregister();
 serviceWorkerRegistration.register();
 reportWebVitals();
