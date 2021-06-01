2b3532e668226e01036b3f90bdc21a524a25532d
Yashwanth M <yashwanth.m@eagleview.com>
2021-06-01 13:22:53 +0530

    msg update

--- src/models/index.js
+++ src/models/index.js
@@ -6,8 +6,7 @@ const apipath = `https://cdn-api.co-vin.in/api`;
 const url = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByPin`
 const zurl = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict`
 const burl = `https://cdn-api.co-vin.in/api/v2/appointment/schedule`
-// const secret = "U2FsdGVkX19mD56KTNfQsZgXJMwOG7u/6tuj0Qvil1LEjx783oxHXGUTDWYm+XMYVGXPeu+a24sl5ndEKcLTUQ==";
-const secret = "U2122FsdGVkX19mD56KTNfQsZgXJMwOG7u/6tuj0Qvil1LEjx783oxHXGUTDWYm+XMYVGXPeu+a24sl5ndEKcLTUQ==";
+const secret = "U2FsdGVkX19mD56KTNfQsZgXJMwOG7u/6tuj0Qvil1LEjx783oxHXGUTDWYm+XMYVGXPeu+a24sl5ndEKcLTUQ==";
 const pollFreq = parseInt(localStorage.pollFreq) || 3250;
 export default class CowinApi {
   req(endpoint) {
