5506872482e76bf723e32bacd973735ed6374c67
Yashwanth M <yashwanth.m@eagleview.com>
2021-06-14 13:06:48 +0530

    revised log with new domain

--- src/logger/index.js
+++ src/logger/index.js
@@ -1,8 +1,10 @@
 import axios from "axios";
+import config from '../models/config'
 
-
-const vaxlogkey = `ZWh8jnnyrX5tBHQwlVJL23Q8IojEfQRQ9uz1qGmf`;
-let url = 'https://prya4emvub.execute-api.us-east-1.amazonaws.com/api';
+// const vaxlogkey = `ZWh8jnnyrX5tBHQwlVJL23Q8IojEfQRQ9uz1qGmf`;
+// let url = 'https://prya4emvub.execute-api.us-east-1.amazonaws.com/api';
+const vaxlogkey = config.key
+let url = config.url + '/log';
 const log = (data) => {
   let opts = {
     headers: {
@@ -10,10 +12,17 @@ const log = (data) => {
     },
   };
   axios.post(url, data, opts).then(response=>{
-    console.log('post', response);
   }).catch(err=>{
     console.info(err);
   })
 }
 
+// axios.options(url).then(response=>{
+//   console.log('post', response);
+// }).catch(err=>{
+//   console.info(err);
+// })
+// log({
+//   location: "testdistrict"
+// })
 export default log;
---
+++ src/models/config.js
@@ -0,0 +1,5 @@
+const config = {
+    key: `vEmJiOi3Ib8xGKvgFdasMayLLzxTz9bS2coRNY9g`,
+    url: "https://yzk6n127nk.execute-api.ap-south-1.amazonaws.com/prod",
+  }; 
+export default config;
