beeac8f9a60d0fe92b8f352f83333e70a4c2e1f3
Yashwanth M <yashwanth.m@eagleview.com>
2021-07-12 16:16:58 +0530

    err version

--- src/App.js
+++ src/App.js
@@ -552,6 +552,7 @@ class App extends React.Component{
             type: "booking_failed",
             errorMessage: desc,
             error: err,
+            build: version
           });
         }         
         
